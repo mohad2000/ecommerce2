db7cad5d8552f3e8b2a8da6c2f6af02423c4be7f
Mohammed <eng.moh.d2000@gmail.com>
2026-06-29 02:45:44 +0300

    auth edit6

--- backend/util/userAuth.js
+++ backend/util/userAuth.js
@@ -5,7 +5,7 @@ export const isAuthenticatedUser = async (req, res, next) => {
     try {
 
 
-        const { token } = req.cookies?.token;;
+        const token = req.cookies?.token;;
 
 
         if (!token) {
